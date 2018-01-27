d8343ee16108eb28c1a61b3edb17097f2e4c23f4
playua18 <playua18@gmail.net>
2018-01-27 03:19:26 +0200

    3th c

--- app/js/main.js
+++ app/js/main.js
@@ -22,8 +22,10 @@ $(window).on('load', function() {
 //   })
 // })
 
-$(document).ready(function() {
-  $('.main-nav__hamb--up, .main-nav__hamb--down').click(function() {
+$(document).ready(function () {
+  $('.main-nav__hamb--up, .main-nav__hamb--down').click(function () {
     $('.main-nav__item:not(.main-nav__item--logo)').slideToggle();
   });
 });
+
+// ||  - оператор или
