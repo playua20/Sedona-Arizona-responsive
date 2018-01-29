7d533a06d8505bd8e54bcf045563af8deba93e9d
playua18 <playua18@gmail.net>
2018-01-29 05:18:12 +0200

    refactor gallery

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,17 @@
-
-$(window).scroll(function() {
+$(window).scroll(function () {
   if ($(this).scrollTop() > $(this).height()) {
     $('.top').addClass('active');
   } else {
     $('.top').removeClass('active');
   }
 });
-$('.top').click(function() {
+$('.top').click(function () {
   $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 });
 
-$(window).on('load', function() {
+$(window).on('load', function () {
   $('.preloader').delay(500).fadeOut('slow');
 });
-
-// $(function() {
-//   $(".animated").on("mouseover", function() {
-//     $(this).addClass("hinge");
-//   }).on("mouseleave", function() {
-//     $(this).removeClass("hinge");
-//   })
-// })
 
 $(document).ready(function () {
   $('.main-nav__hamb--up, .main-nav__hamb--down').click(function () {
@@ -28,31 +19,4 @@ $(document).ready(function () {
   });
 });
 
-$(document).ready(function() {
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    $('#view').setZoomPicture({
-      thumbsContainer: '#pics-thumbs',
-      prevContainer: '#nav-left-thumbs',
-      nextContainer: '#nav-right-thumbs',
-      loadMsg: 'Loading...'
-    });
-  }
-  // else  if (window.matchMedia("(min-width: 768px)").matches) {
-  //   $('#view').setZoomPicture({
-  //     thumbsContainer: '#pics-thumbs',
-  //     prevContainer: '#nav-left-thumbs',
-  //     nextContainer: '#nav-right-thumbs',
-  //     zoomContainer: '#zoom',
-  //     loadMsg: 'Loading...'
-  //   });
-  // };
-});
-
-// $(document).ready(function() {
-//   $('#view').setZoomPicture({
-//     if (window.matchMedia("(min-width: 768px)").matches) {
-//     zoomContainer: '#zoom'
-//   }
-//   });
-// });
-// ||  - оператор или
+jQuery("#gallery").unitegallery();
